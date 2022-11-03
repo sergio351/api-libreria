1f9a90230d703f143833bf3d74873fdbd8e756ba
sergio351 <velazquezsergio714@gmail.com>
2022-11-03 10:22:49 -0300

    first commit

---
+++ apiautores/Controllers/AutoresController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Autor>>> Get()
+        {
+            return await context.Autores.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor)
+        {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+
+        }
+
+
+        [HttpPut("{id:int}")]//api/autores/1
+        public async Task<ActionResult> Put(Autor autor, int id)
+        {
+           if (autor.Id != id)
+            {
+                return BadRequest("el id del autor no coincide con la id de la url");
+            }
+
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+            if (!existe)
+            {
+                return NotFound("error");
+            }
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+
+        }
+
+        [HttpDelete("{id:int}")]//api/autores/1
+        public async Task<ActionResult> Delete(int id)
+        {
+            var  existe = await context.Autores.AnyAsync(x => x.Id == id);  
+            if (!existe)
+            {
+                return NotFound("no se encontro id");
+            }
+
+            context.Remove(new Autor() { Id = id});
+            await context.SaveChangesAsync();
+            return Ok();
+
+        }
+
+    }
+}
